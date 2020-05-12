dde3779cd3fed55bc9ece8844dde726f25bb5c4b
Homarv <pierratmarvin3@gmail.com>
2020-05-12 16:50:47 +0200

    17h

--- nomdeta/app/models/event.rb
+++ nomdeta/app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
     has_many :attendances
     has_many :users, through: :attendances
+
+    def welcome_event_send
+        UserMailer.welcome_event_email(self).deliver_now
+      end
 end
