0b2ca6c3ecd8bb7e23cb2fa21b68e41d057aeb06
Homarv <pierratmarvin3@gmail.com>
2020-05-12 03:10:46 +0200

    ça avance

--- nomdeta/db/migrate/20200511142941_create_users.rb
+++ nomdeta/db/migrate/20200511142941_create_users.rb
@@ -1,10 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-
+      t.string :first_name 
+      t.string :email, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.text :description
+      t.string :last_name
       t.timestamps
+
     end
   end
 end
--- nomdeta/db/seeds.rb
+++ nomdeta/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Event.destroy_all
+
+
+10.times do
+  event = Event.create(
+    start_date: Faker::Date.between(from: Date.today, to: 2.year.from_now),
+    duration: Faker::Number.between(from: 1, to: 1000),
+    title: Faker::Games::WorldOfWarcraft.quote,
+    description: Faker::Lorem.sentence(word_count: 3),
+    price: Faker::Number.between(from: 1, to: 1000),
+    location: Faker::Nation.capital_city,
+  )
+end
+
+10.times do
+  user = User.create(
+    first_name: Faker::JapaneseMedia::DragonBall.character,
+    last_name: Faker::JapaneseMedia::OnePiece.island,
+    description: Faker::Hipster.sentence,
+    )
+end
+
+
+20.times do 
+  attendance = Attendance.create(
+    stripe_customer_id: Faker::Stripe.valid_card,
+    )
+end
