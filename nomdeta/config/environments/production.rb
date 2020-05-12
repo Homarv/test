53352c965734d285e34904072f00b262f409b39f
Homarv <pierratmarvin3@gmail.com>
2020-05-12 23:20:55 +0200

    Yolo

--- nomdeta/config/environments/production.rb
+++ nomdeta/config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { :host => 'frozen-temple-66558.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
